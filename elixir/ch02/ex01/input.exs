6f0af3d041a66e601e23b4a38339dd1d26889047
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2015-08-08 13:04:00 +0200

    Keep the input, string concatenation, and output separate.

--- elixir/ch02/ex01/input.exs
+++ elixir/ch02/ex01/input.exs
@@ -1,11 +1,18 @@
-defmodule Ex01 do
-  def greet do
-    IO.gets("What is your name? ")
-    |> String.strip
+defmodule Greeter do
+  def ask_and_greet do
+    ask_name
+    |> sanitize_input
+    |> create_greet_message
     |> greet
   end
 
-  defp greet(name), do: IO.puts("Hello, #{name}, nice to meet you!")
+  def ask_name, do: IO.gets("What is your name? ")
+
+  def sanitize_input(input), do: String.strip(input)
+
+  def create_greet_message(name), do: "Hello, #{name}, nice to meet you!"
+
+  def greet(name), do: IO.puts(name)
 end
 
-Ex01.greet
+Greeter.ask_and_greet
